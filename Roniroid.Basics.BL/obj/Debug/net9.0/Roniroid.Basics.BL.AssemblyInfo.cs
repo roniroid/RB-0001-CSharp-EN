8c99482f1d7daf171701ae22100ebfaf4fa8abe9
roniroid <ronal.dj@hotmail.es>
2025-05-18 17:30:55 -0600

    Added many files

--- Roniroid.Basics.BL/obj/Debug/net9.0/Roniroid.Basics.BL.AssemblyInfo.cs
+++ Roniroid.Basics.BL/obj/Debug/net9.0/Roniroid.Basics.BL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Roniroid.Basics.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b269ca2c048eff926b7f9367acd5e98cdd77fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Roniroid.Basics.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Roniroid.Basics.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
